e7e748a33d58f84d776780f2accde21fd7656684
melissayauri <yaurimelissa@gmail.com>
2018-04-30 18:23:38 -0500

    incorporando boton guardar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
-/* Inicializando el logeo mediante gmail */
-var provider = new firebase.auth.GoogleAuthProvider();
-$('#login').click(function() {
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    console.log(result.user);
+$(document).ready(function() {
+  /* Inicializando firebase*/
+  var config = {
+    apiKey: 'AIzaSyBBur9iGdP0N0vlWlRnWxPjyMGR2nnPV90',
+    authDomain: 'fir-gmd-pruebas.firebaseapp.com',
+    databaseURL: 'https://fir-gmd-pruebas.firebaseio.com',
+    projectId: 'fir-gmd-pruebas',
+    storageBucket: 'fir-gmd-pruebas.appspot.com',
+    messagingSenderId: '701971426047'
+  };
+  firebase.initializeApp(config);
+  /* Inicializando el logeo mediante gmail */
+  var provider = new firebase.auth.GoogleAuthProvider();
+  /* evento para el botón logeo */
+  $('#login').click(function() {
+  /* Aparece la ventana del logueo */
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+    // console.log(result.user);
+      $('#login').hide();
+      console.log(result.user);
+      var name = `<p>${result.user.displayName}</p>`;
+      /* muestra el nombre al terminar de inciar sesión */
+      $('#root').append(name);
+    });
   });
+  /* guardando los datos */
+  
 });
