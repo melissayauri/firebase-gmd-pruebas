16393f14c4d6afc4a80b1ce42b5ce057a9935cd9
melissayauri <yaurimelissa@gmail.com>
2018-05-02 09:59:08 -0500

    incorporando base de datos con login

--- js/app.js
+++ js/app.js
@@ -18,15 +18,29 @@ $(document).ready(function() {
     // console.log(result.user);
       $('#login').hide();
       console.log(result.user);
+      saveDate(result.user);
       var name = `<p>${result.user.displayName}</p>`;
       /* muestra el nombre al terminar de inciar sesión */
       $('#root').append(name);
     });
   });
+
+  /* guardando datos del logeo */
+
+  function saveDate(user) {
+    var dataUser = {
+      name: user.displayName,
+      mail: user.email,
+      uid: user.uid
+    };
+    /* se aplica push para agregar a cada usuario */
+    firebase.database().ref('usuarios').push(dataUser);
+  };
   /* guardando los datos */
   $('#save').click(function() {
     alert('listo');
     /* Los datos se almacenan en la rama prueba */
+    /* set actualzia toda la rama */
     firebase.database().ref('prueba').set({
       nombre: 'melissa',
       edad: '27 años',
